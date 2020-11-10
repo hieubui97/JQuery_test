7af1e7305d7ee351664203ef0642748e18bb86b0
Hiếu Bùi <khuakhuaqt@gmail.com>
2020-11-10 17:44:57 +0700

    initial commit

---
+++ JQuery_test/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JQuery_test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ JQuery_test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JQuery_test.MvcApplication" Language="C#" %>
